91706d6ee02102395f0a21262c83561e332dfb4c
lichunyan <lichunyan8@100tal.com>
2020-05-29 18:31:06 +0800

    mobile埋下冲突

---
+++ src/mobile/menu/BottomNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in menuList" :key="item.path">{{item.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menuList: [
+        { title: '环境健康1', path: '/health1' },
+        { title: '环境健康2', path: '/health2' },
+        { title: '环境健康3', path: '/health3' },
+        { title: '环境健康4', path: '/health4' }
+      ]
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
